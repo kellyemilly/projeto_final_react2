7e077dc2109efc598acde0f0b9eebadcf88383a9
kelly emilly <kellyemilly178@gmail.com>
2024-02-15 13:02:55 -0300

    atualizando

---
+++ src/componentes/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import styles from './footer.module.css'
+
+function Footer() {
+  return(
+    <footer className={styles.footer}>
+      <p>
+        Criado por Kelly Emilly  
+🥰
+      
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
